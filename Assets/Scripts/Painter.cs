b36170f22011c4446f61ff6b82978fb6289f7617
Martynas <135633452+MartynasOnTop@users.noreply.github.com>
2023-10-26 22:08:21 +0300

    Color

--- Assets/Scripts/Painter.cs
+++ Assets/Scripts/Painter.cs
@@ -7,6 +7,10 @@ public class Painter : MonoBehaviour
     public GameObject lineObject;
     LineRenderer line;
 
+    public GameObject Red;
+    public GameObject Blue;
+    public GameObject Yellow;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -26,5 +30,23 @@ public class Painter : MonoBehaviour
         worldPos.z = 0;
 
         line.SetPosition(line.positionCount - 1, worldPos);
+    }
+    void OnMouseDown()
+    {
+        if (gameObject.tag == "Red")
+        {
+            line.startColor = Color.red;
+            line.endColor = Color.red;
+        }
+        if (gameObject.tag == "Blue")
+        {
+            line.startColor = Color.blue;
+            line.endColor = Color.blue;
+        }
+        if (gameObject.tag == "Yellow")
+        {
+            line.startColor = Color.yellow;
+            line.endColor = Color.yellow;
+        }
     }
 }
